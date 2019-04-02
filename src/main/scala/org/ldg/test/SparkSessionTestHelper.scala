5720f792b5c1e24a13401f43faef1bacccb65a4d
Lance Gatlin <lance.gatlin@gmail.com>
2019-04-02 17:21:44 -0400

    work on SparkSessionTestHelper

--- src/main/scala/org/ldg/test/SparkSessionTestHelper.scala
+++ src/main/scala/org/ldg/test/SparkSessionTestHelper.scala
@@ -1,14 +1,38 @@
 package org.ldg.test
 
 import com.typesafe.scalalogging.slf4j.LazyLogging
+import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
+import scala.collection.JavaConverters._
 import scala.io.Source
 import org.ldg._
 
+object SparkSessionTestHelper extends LazyLogging {
+
+  val sparkSessionCache = new TSMap[SparkTestConfig,SparkSession]
+
+  case class SparkTestConfig(
+    name: String,
+    config: Seq[(String,String)]
+  )
+
+  Runtime.getRuntime.addShutdownHook(new Thread {
+    override def run(): Unit = {
+      sparkSessionCache.entrySet().asScala.foreach { entry =>
+        val sparkTestCfg = entry.getKey
+        val sparkSession = entry.getValue
+        logger.info(s"Closing SparkSession for config ${sparkTestCfg.name}")
+        sparkSession.close()
+      }
+    }
+  })
+}
+
 trait SparkSessionTestHelper extends FileTestHelper with LazyLogging {
-  def appName: String = this.getClass.getName
-  def sparkCfg: Seq[(String,String)] = Seq.empty
+  import SparkSessionTestHelper._
+
+  def sparkTestCfg: SparkTestConfig = SparkTestConfig("default", Seq.empty)
 
   /**
     * Construct a SparkSession, pass to provided function, then close the session
@@ -18,36 +42,23 @@ trait SparkSessionTestHelper extends FileTestHelper with LazyLogging {
     * @tparam A return type
     * @return value returned by function
     */
-  def withSparkSession[A](f: SparkSession => A) : A =
-    withSparkSession()(f)
+  def withSparkSession[A](f: SparkSession => A) : A = {
+    val sparkSession =
+      sparkSessionCache.getOrCompute(sparkTestCfg) { () =>
+          val _sparkSession =
+            SparkSession
+              .builder()
+              .master("local[*]")
+              .appName("SparkTest")
 
-  /**
-    * Construct a SparkSession, pass to provided function, then close the session
-    * even if an exception is thrown
-    *
-    * @param extraCfg (key,value) configuration pairs to configure SparkSession
-    * @param f function to pass SparkSession to
-    * @tparam A return type
-    * @return value returned by function
-    */
-  def withSparkSession[A](extraCfg: (String,String)*)(f: SparkSession => A) : A = {
-    val _sparkSession = SparkSession
-      .builder()
-      .master("local[*]")
-      .appName(appName)
+          sparkTestCfg.config.foreach { case (k,v) =>
+            _sparkSession.config(k,v)
+          }
+          logger.info(s"Creating SparkSession[${sparkTestCfg.name}]")
+          _sparkSession.getOrCreate()
+      }
 
-    (sparkCfg ++ extraCfg).foreach { case (k,v) =>
-      _sparkSession.config(k,v)
-    }
-    logger.info("Creating SparkSession")
-    val sparkSession = _sparkSession.getOrCreate()
-    val retv = try {
-      f(sparkSession)
-    } finally {
-      logger.info("Closing SparkSession")
-      sparkSession.close()
-    }
-    retv
+    f(sparkSession)
   }
 
   /**
@@ -101,4 +112,20 @@ trait SparkSessionTestHelper extends FileTestHelper with LazyLogging {
     }
   }
 
+  def csvToDF(
+    csv: String,
+    schema: StructType
+  )(implicit
+    sparkSession: SparkSession
+  ) : DataFrame = {
+    import sparkSession.sqlContext.implicits._
+
+    val csvLines = csv.lines.toList
+    val csvDS =
+      sparkSession.sparkContext
+        .parallelize(csvLines)
+        .toDS
+
+    sparkSession.read.schema(schema).option("header","true").csv(csvDS)
+  }
 }
